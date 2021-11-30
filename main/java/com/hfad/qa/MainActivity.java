900d8fffed6c90ccc78d66654ba78c0d9f78e2e4
SashaStark <84183933+SashaStarkova@users.noreply.github.com>
2021-11-30 16:26:21 +0500

    30/11

--- main/java/com/hfad/qa/MainActivity.java
+++ main/java/com/hfad/qa/MainActivity.java
@@ -2,12 +2,29 @@ package com.hfad.qa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonRegistration = (Button) findViewById(R.id.registration);
+        buttonRegistration.setOnClickListener(this);
+        Button buttonEntry = (Button) findViewById(R.id.entry);
+        buttonEntry.setOnClickListener(this::OnClick1);
     }
+    @Override
+    public void onClick (View view)
+    {Intent i;
+    i= new Intent(this, Registration.class);
+    startActivity(i);}
+
+    public void OnClick1 (View view)
+    {Intent b;
+        b= new Intent(this, Entry.class);
+        startActivity(b);}
 }
