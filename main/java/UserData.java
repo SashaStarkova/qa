e15fdc11eded50255e14fe05f2fb852d390e1291
SashaStark <84183933+SashaStarkova@users.noreply.github.com>
2021-12-07 12:42:53 +0500

    7.12

---
+++ main/java/UserData.java
@@ -0,0 +1,13 @@
+public class UserData {
+    public String id, nickname, phone_num, password;
+
+    public UserData(){
+    }
+
+    public UserData(String id, String nickname, String phone_num, String password) {
+        this.id = id;
+        this.nickname = nickname;
+        this.phone_num = phone_num;
+        this.password = password;
+    }
+}
--- main/java/com/hfad/qa/Registration.java
+++ main/java/com/hfad/qa/Registration.java
@@ -1,13 +1,31 @@
 package com.hfad.qa;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Registration extends AppCompatActivity {
-
+    private EditText nickname, phone_num, password;
+    private DatabaseReference MyDatabase;
+    private String USER_KEY = "User";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        init();
+    }
+    public void init()
+    {nickname= findViewById(R.id.nickname);
+    phone_num=findViewById(R.id.phone_num);
+    password=findViewById(R.id.password);
+    MyDatabase= FirebaseDatabase.getInstance().getReference(USER_KEY);
+    }
+    public void onClickSave(View view){
+
     }
 }
